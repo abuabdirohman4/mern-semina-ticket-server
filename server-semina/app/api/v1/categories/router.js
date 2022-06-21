f042f76ba48a526834a8e2e3b427ece0e6e0cb26
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-06-21 15:12:36 +0700

    add : day 3 - folder & file setup

---
+++ server-semina/app/api/v1/categories/router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express();
+
+router.get('/categories', (req, res) => {
+    console.log(req.query);
+    
+    res.status(200).json({
+        message: 'Halaman categories',
+    });
+});
+
+module.exports = router;
---
+++ server-semina/index.js
@@ -0,0 +1,7 @@
+function add() {
+    var a = 1;
+    let b = 1;
+};
+
+console.log(a);
+console.log(b);
