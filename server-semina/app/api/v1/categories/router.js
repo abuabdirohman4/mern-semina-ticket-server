0113b9253a0e4ed2a2d09714a484f8a8d2a5aca5
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-16 17:21:32 +0700

    day 7 : implement auth role on categories [done]

--- server-semina/app/api/v1/categories/router.js
+++ server-semina/app/api/v1/categories/router.js
@@ -26,14 +26,14 @@ const {authenticateUser, authorizeRoles } = require('../../../middlewares/auth')
 //     });
 // });
 
-router.get('/categories', authenticateUser, index);
+router.get('/categories', authenticateUser, authorizeRoles('organizer'), authorizeRoles('organizer'), index);
 
-router.post('/categories', authenticateUser, create);
+router.post('/categories', authenticateUser, authorizeRoles('organizer'), create);
 
-router.get('/categories/:id', authenticateUser, find);
+router.get('/categories/:id', authenticateUser, authorizeRoles('organizer'), find);
 
-router.put('/categories/:id', authenticateUser, update);
+router.put('/categories/:id', authenticateUser, authorizeRoles('organizer'), update);
 
-router.delete('/categories/:id', authenticateUser, destroy);
+router.delete('/categories/:id', authenticateUser, authorizeRoles('organizer'), destroy);
 
 module.exports = router;
