b35c1213638f5df49ed033f383c43caed74f1220
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-16 18:43:28 +0700

    day 8 : update service events & talents

--- server-semina/app/services/mongoose/events.js
+++ server-semina/app/services/mongoose/events.js
@@ -9,7 +9,7 @@ const { NotFoundError, BadRequestError } = require('../../errors');
 
 const getAllEvents = async (req) => {
     const { keyword, category, talent } = req.query;
-    let condition = {};
+    let condition = { organizer: req.user.organizer };
 
     if (keyword) {
         condition = { ...condition, title: { $regex: keyword, $options: 'i' } };
@@ -37,7 +37,7 @@ const getAllEvents = async (req) => {
             select: '_id name role image',
             populate: { path: 'image', select: '_id  name' },
         });
-        // }).select('_id title');
+    // }).select('_id title');
 
     return result;
 };
@@ -81,6 +81,7 @@ const createEvents = async (req) => {
         image,
         category,
         talent,
+        organizer: req.user.organizer,
     });
 
     return result;
@@ -89,7 +90,10 @@ const createEvents = async (req) => {
 const getOneEvents = async (req) => {
     const { id } = req.params;
 
-    const result = await Events.findOne({ _id: id })
+    const result = await Events.findOne({
+        _id: id,
+        organizer: req.user.organizer
+    })
         .populate({ path: 'image', select: '_id name' })
         .populate({
             path: 'category',
@@ -130,7 +134,8 @@ const updateEvents = async (req) => {
 
     // cari even bedasarkan field id
     const checkEvent = await Events.findOne({
-        _id: id ,
+        _id: id,
+        organizer: req.user.organizer,
     });
 
     // jika id result false / null maka akan menampilkan error `Tidak ada event dengan id` yang dikirim client
@@ -159,6 +164,7 @@ const updateEvents = async (req) => {
             image,
             category,
             talent,
+            organizer: req.user.organizer
         },
         { new: true, runValidators: true }
     );
@@ -174,6 +180,7 @@ const deleteEvents = async (req) => {
 
     const result = await Events.findOne({
         _id: id,
+        organizer: req.user.organizer,
     });
 
     if (!result)
