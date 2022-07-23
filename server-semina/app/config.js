2a14a42aff1f0d82c5f5c43a33d69974e969ad65
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-23 13:57:45 +0700

    day 9 : update env & config

--- server-semina/app/config.js
+++ server-semina/app/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
     urlDb: process.env.URL_MONGODB_DEV,
     jwtSecret: process.env.SECRET_KEY,
-    jwtExpiration: '24h', // 24 Hours
-    gmail: 'abuabdirohman4@gmail.com',
-    password: 'qycrncjnxviququj', // Password device
+    jwtExpiration: process.env.JWT_EXPIRATION, // 24 Hours
+    gmail: process.env.GMAIL,
+    password: process.env.PASSWORD, // Password device
 }
