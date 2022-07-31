31cf903237e6329b1e265092c8fdbbfc17c5c99f
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-31 20:03:20 +0700

    day 12 : install cors

--- server-semina/app.js
+++ server-semina/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const app = express();
 
 // Import Router
@@ -20,6 +20,7 @@ const participantsRouter = require('./app/api/v1/participants/router');
 const notFoundMiddleware = require('./app/middlewares/not-found');
 const handleErrorMiddleware = require('./app/middlewares/handler-error');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
